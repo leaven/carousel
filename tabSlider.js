9d112a9bf16ed5635095e064f713e93ff5e8bb26
liwei24 <liwei24@baidu.com>
2014-08-05 11:03:08 +0800

    tabSlider.js changed by leaven

--- tabSlider.js
+++ tabSlider.js
@@ -3,12 +3,11 @@
 	* @param  {object}      options
 	* @config {Number}      orientation 描述该组件是创建一个横向滚动组件或是竖向滚动组件，取值：{horizontal: 横向, vertical: 竖向}，默认是horizontal.
 	* @config {Number}      originalIndex 默认选项卡的聚焦项，默认0. 
-	* @config {Number}      viewSize 
 	* @config {String}      {direction}   {backward|forward}
-	* @config {Boolean}     autoSlider
-	* @config {String}      {animation}   {slide|fade}
-	* @config {Number}      {interval}   default 5000
-	* @config {Number}      {duration}   default 500
+	* @config {Boolean}     autoSlider    默认滚动,默认true
+	* @config {String}      {animation}   动画样式，{slide|fade}，默认slide
+	* @config {Number}      {interval}   两次滚动间隔，默认 5000
+	* @config {Number}      {duration}   每次滚动时间，默认 500
 
 
 
